bc82b7da39be0fdf206e8c901112a01f3b0e922c
Cuza <baopham92@yahoo.com>
2019-09-09 19:10:03 -0400

    correction

--- src/App.js
+++ src/App.js
@@ -28,24 +28,24 @@ function App() {
         <div className="homeButtons">
           <button
             className="homeButtons__touchdown"
-            onClick={() => setHomeScore(homeScore + 6)}>
+            onClick={() => setHomeScore(homeScore + 7)}>
             Home Touchdown
           </button>
           <button
             className="homeButtons__fieldGoal"
-            onClick={() => setHomeScore(homeScore + 1)}
+            onClick={() => setHomeScore(homeScore + 3)}
           >Home Field Goal
           </button>
         </div>
         <div className="awayButtons">
           <button
             className="awayButtons__touchdown"
-            onClick={() => setAwayScore(awayScore + 6)}>
+            onClick={() => setAwayScore(awayScore + 7)}>
             Away Touchdown
             </button>
           <button
             className="awayButtons__fieldGoal"
-            onClick={() => setAwayScore(awayScore + 1)}>
+            onClick={() => setAwayScore(awayScore + 3)}>
             Away Field Goal
             </button>
         </div>
